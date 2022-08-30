6f4fc280e4fd1bd5513eb80d3a812f819aaadb42
Zachary Hamilton <zdhamilton@confluent.io>
2022-08-30 10:58:11 -0400

    replaced mysql with postgres

--- postgres/products/setup.sql
+++ postgres/products/setup.sql
@@ -47,7 +47,7 @@ CREATE TABLE orders (
 
 CREATE PROCEDURE generate_orders() AS $$
 BEGIN
-    -- for ultimate coolness, use implement a sine function in the future to fluctuate the orders per second
+    -- for ultimate coolness, implement a sine function in the future to fluctuate the orders per second
     WHILE 1 = 1 LOOP
         DECLARE 
             product products.products%ROWTYPE;
@@ -57,7 +57,7 @@ BEGIN
             SELECT * INTO product FROM products.products ORDER BY random() LIMIT 1; 
             SELECT * INTO customer FROM products.customers ORDER BY random() LIMIT 1;
             SELECT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring) INTO uuid;
-            RAISE NOTICE 'values are product % customer % and order %', product.product_id, customer.id, uuid;
+            -- RAISE NOTICE 'values are product % customer % and order %', product.product_id, customer.id, uuid;
             INSERT INTO products.orders (order_id, product_id, customer_id) VALUES (uuid, product.product_id, customer.id);
             COMMIT;
             PERFORM pg_sleep(1);
@@ -92,7 +92,7 @@ BEGIN
             END IF;
 
             UPDATE products.products SET price = CONCAT('$', CAST(new_price AS VARCHAR)) WHERE product_id = id;
-            RAISE NOTICE 'product_id: %, old_price: %, price_delta: %, new_price: %', id, old_price, price_delta, new_price;
+            -- RAISE NOTICE 'product_id: %, old_price: %, price_delta: %, new_price: %', id, old_price, price_delta, new_price;
             COMMIT;
             PERFORM pg_sleep(150);
         END;
